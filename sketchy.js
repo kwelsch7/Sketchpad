f6d290011cd8467f91911f6237f04582bb5faa36
Konnor Welsch <kwelsch@2ndwatch.com>
2017-07-03 11:15:24 -0700

    Added JS to create the grid and color squares on mouseenter

--- sketchy.js
+++ sketchy.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-    
+    createGrid();
+    $("#canvas").on("mouseenter", "div div", colorSquare);
 });
+
+function createGrid(dimX, dimY) {
+    if(dimX == null) {
+        dimX = 66;
+    }
+    if(dimY == null) {
+        dimY = 33;
+    }
+
+    for(let y = 0; y < dimY; y++) {
+        let row = $('<div class="grid-row"></div>');
+
+        for(let x = 0; x < dimX; x++) {
+            row.append('<div class="grid-square"></div>');
+        }
+        $("#canvas").append(row);
+    }
+}
+
+function colorSquare() {
+    $(this).addClass("colored");
+}
